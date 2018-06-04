f58a449f678359c9b70fdcae8d5821f6774de1aa
Albert Lee <albertlee@vector.com.my>
2018-06-04 10:21:30 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+//var path = __dirname + '/';
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+  //res.render(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use(express.static('views'));
+app.use(express.static('css'));
+app.use(express.static('fonts'));
+app.use(express.static('js'));
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
